05652b336df12fbc38fab33ab1f4045b2ef94868
Wasesatama <varyo44@gmail.com>
2023-07-24 15:41:34 +0700

    change sql pass

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("testsql","root","mantap18","mantap");
+$conn = mysqli_connect("testsql","root","yohan12345","mantap");
 
 if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
@@ -7,10 +7,10 @@ if ($conn->connect_error){
 echo "Connected to mySQL Sucessfully!!!";
 
 //query to database
-$sql = "SELECT name FROM account WHERE id = '1'";
+$sql = "SELECT name FROM account WHERE id = '2'";
 $result = $conn->query($sql);
 
 //print to web
 $row = $result->fetch_assoc();
-        echo "<br>Hello, " . $row["name"] . ". Have a nice day folks";
+        echo "<br>Hello, " . $row["name"] . ". Have a nice day bro! From: Aryo";
 ?>
--- index2.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect("testsql","root","mantap18");
-
-if ($conn->connect_error){
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected to mySQL Sucessfully!!!";
-?>
